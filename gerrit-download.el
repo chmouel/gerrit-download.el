76d743c876f5407e1f0a55ffc99ef89a638bdf71
Chmouel Boudjnah <chmouel@chmouel.com>
2013-08-30 21:02:10 +0200

    Documentation is hard.

--- gerrit-download.el
+++ gerrit-download.el
@@ -37,7 +37,10 @@
 ;;; This is using magit and the `magit-repo-dirs` variable to download
 ;;; the change into.
 ;;;
-;;; With gnus add a
+;;; With gnus add a hook like this
+;;; (add-hook 'gnus-startup-hook 'gerrit-download-insinuate-gnus)
+;;; in your init file and use the 'v' key to have it automatically
+;;; parse the email and show the diff.
 
 ;;; ToDo:
 ;; magit interface key.
